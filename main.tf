1c887221de33b1d876839b4795355cc258f04c9e
SAITEJGIT <saiteja982610kommineni@gmail.com>
2024-09-08 19:28:55 +0530

    test

--- main.tf
+++ main.tf
@@ -1,23 +1,14 @@
 terraform {
-
   required_providers {
-
     aws = {
-
       source = "hashicorp/aws"
-
       version = "5.66.0"
-
     }
-
   }
-
 }
 
 provider "aws" {
-
   region  = var.aws_region
-
 }
 
 resource "aws_instance" "app_server" {
